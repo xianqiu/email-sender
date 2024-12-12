216dc109975685e7dd0e519dd3c64ef5ee952a6c
xianqiu <x.qiu@qq.com>
2024-12-12 18:46:52 +0800

    - add example files

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ if __name__ == '__main__':
     user = 'data/user_qiu/qiu.json'
     template = 'data/user_qiu/letter.txt'
 
-    #Runner.run_sender(user, template,
-    #                  ['x.qiu@qq.com', 'qiu@bravebird.co.jp'])
+    # Runner.run_sender(user, template,[''])
 
     job_path = 'data/user_qiu/job_test.csv'
     Runner.run_job_sender(job_path, user, template)
